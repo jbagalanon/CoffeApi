c819559f71ecfeec0496902aa21438191f48605e
Jomskie <jbagalanon@gmail.com>
2020-08-12 03:45:48 +0800

    Initialiaze migration

---
+++ ExpressoApi/Migrations/202008111939462_InitialCreate.cs
@@ -0,0 +1,60 @@
+﻿namespace ExpressoApi.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Menus",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Image = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.SubMenus",
+                c => new
+                    {
+                        SubMenuId = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Description = c.String(),
+                        Price = c.String(),
+                        Image = c.String(),
+                        Menu_Id = c.Int(),
+                    })
+                .PrimaryKey(t => t.SubMenuId)
+                .ForeignKey("dbo.Menus", t => t.Menu_Id)
+                .Index(t => t.Menu_Id);
+            
+            CreateTable(
+                "dbo.Reservations",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false),
+                        Email = c.String(nullable: false),
+                        Phone = c.String(nullable: false),
+                        TotalPeople = c.Int(nullable: false),
+                        Date = c.DateTime(nullable: false),
+                        Time = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.SubMenus", "Menu_Id", "dbo.Menus");
+            DropIndex("dbo.SubMenus", new[] { "Menu_Id" });
+            DropTable("dbo.Reservations");
+            DropTable("dbo.SubMenus");
+            DropTable("dbo.Menus");
+        }
+    }
+}
---
+++ ExpressoApi/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace ExpressoApi.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ExpressoApi.Data.ExpressoDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "ExpressoApi.Data.ExpressoDbContext";
+        }
+
+        protected override void Seed(ExpressoApi.Data.ExpressoDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
